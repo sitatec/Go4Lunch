be232f17fdd50804a3dd2558f5f6bc9146d5043f
Sita Bérété <sita.berete.3@gmail.com>
2021-05-29 14:31:07 -0400

    Feature: account deletion confirmation dialog

--- app/src/main/java/com/berete/go4lunch/ui/settings/SettingsFragment.java
+++ app/src/main/java/com/berete/go4lunch/ui/settings/SettingsFragment.java
@@ -1,9 +1,13 @@
 package com.berete.go4lunch.ui.settings;
 
+import android.app.AlertDialog;
+import android.app.Dialog;
 import android.os.Bundle;
 import android.view.Menu;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.DialogFragment;
 import androidx.preference.CheckBoxPreference;
 import androidx.preference.Preference;
 import androidx.preference.PreferenceFragmentCompat;
@@ -67,6 +71,12 @@ public class SettingsFragment extends PreferenceFragmentCompat {
   }
 
   private boolean onDeleteAccountClick(Preference __) {
+    new DeleteConfirmationDialog(this::deleteUserAccount)
+        .show(getParentFragmentManager(), getString(R.string.delete_confirmation_dialog_TAG));
+    return true;
+  }
+
+  private void deleteUserAccount() {
     userProvider.deleteCurrentUserAccount(
         new Callback<Boolean>() {
           @Override
@@ -89,7 +99,6 @@ public class SettingsFragment extends PreferenceFragmentCompat {
                 .show();
           }
         });
-    return true;
   }
 
   @Override
@@ -97,11 +106,34 @@ public class SettingsFragment extends PreferenceFragmentCompat {
     if (preference instanceof TimePreference) {
       final TimePreferencePickerDialog dialog =
           TimePreferencePickerDialog.newInstance(preference.getKey());
+      // The Fragment.setTargetFragment() method is deprecated but for now, I have to use it
+      // because the latest version of androidx.preference (1.1.1 at this time) uses the
+      // Fragment.getTargetFragment() in the onCreate method of PreferenceDialogFragmentCompat which
+      // require the call of the methode below.
       dialog.setTargetFragment(this, 0);
-      //      dialog.getParentFragmentManager().setFragmentResultListener(dialo);
-      dialog.show(getParentFragmentManager(), "The time preference picker dialog");
+      dialog.show(getParentFragmentManager(), getString(R.string.time_preference_dialog_TAG));
     } else {
       super.onDisplayPreferenceDialog(preference);
+    }
+  }
+
+  public static class DeleteConfirmationDialog extends DialogFragment {
+
+    private final Runnable onPositiveButtonClicked;
+
+    public DeleteConfirmationDialog(Runnable onPositiveButtonClicked) {
+      this.onPositiveButtonClicked = onPositiveButtonClicked;
+    }
+
+    @NonNull
+    @Override
+    public Dialog onCreateDialog(@Nullable Bundle savedInstanceState) {
+      return new AlertDialog.Builder(requireContext())
+          .setTitle(R.string.delete_confirmation_dialogue_title)
+          .setMessage(R.string.ask_account_deletion_confirmation)
+          .setNegativeButton(R.string.cancel_txt, (dialog, which) -> dismiss())
+          .setPositiveButton(R.string.delete_txt, (dialog, which) -> onPositiveButtonClicked.run())
+          .create();
     }
   }
 }
